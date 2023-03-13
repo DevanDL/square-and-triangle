8a466d1ab49359f820bbb6a201c2eed77cfa0e4d
DevanDL <127772011+DevanDL@users.noreply.github.com>
2023-03-13 22:59:28 +0700

    Add files via upload

---
+++ bentuk.java
@@ -0,0 +1,64 @@
+package sessionn3;
+
+import java.util.Scanner;
+
+public class bentuk {
+		public static void triangle(int rows){
+			int spaces = rows - 1;
+			
+			for(int i = 1; i<=rows; i++) {
+				for(int j = 1; j<=spaces; j++) {
+					System.out.print(" ");
+				}
+				spaces--;
+				
+				for(int j = 1 ; j<=2*i-1; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+
+		public static void circle(int args){
+			
+		}
+
+		public static void square(int rows){
+			for(int i = 1 ;i<=rows ; i++) {
+				for(int j = 1; j<=rows ;j++) {
+					if(i!=1 && i!=rows && j!=1 && j!=rows) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					}
+					
+				}
+				System.out.println();
+			}
+		}
+		
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Shape: ");
+			String string = input.nextLine();
+			
+			if(string.equals("square")) {
+				System.out.print("Size: ");
+				int size = input.nextInt();
+				square(size);
+			} else if(string.equals("triangle")) {
+				System.out.print("Size: ");
+				int size = input.nextInt();
+				triangle(size);
+			} else if (string.equals("circle")) {
+				System.out.print("Size: ");
+				int size = input.nextInt();
+				circle(size);
+			} else {
+				System.out.println("not valid");
+			}
+			
+			input.close();
+		}
+	
+}
